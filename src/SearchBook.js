4fe0908ea66a23a7b5199a96e3a54ba8c4603c85
MujunZ <o.6180339@gmail.com>
2017-07-23 20:03:54 -0400

    Add BooksAPI and searchBooks function
    TODO:
    - Search all, instead of only titles
    - Add book to booklist

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -5,26 +5,44 @@ import * as BooksAPI from './BooksAPI';
 
 class SearchBook extends Component {
 	state = {
-		foundBooks: []
+		foundBooks: [],
+		searchedBooks: [],
+		query: ""
 	}
 	componentDidMount() {
 	  BooksAPI.getAll().then(foundBooks => {
-	  	this.setState({ foundBooks });
-	  	console.log(this.state);
+	  	this.setState( state => { state.foundBooks = foundBooks });
 	  })
 	}
+
+	searchBooks = query => {
+	  	let result = [];
+	  	let searchTerm = query.toLowerCase();
+	  	if(query)
+	  		result = this.state.foundBooks.filter(book => book.title.toLowerCase().includes(searchTerm));
+	  	this.setState( state => {
+	  		state.searchedBooks = result;
+	  		state.query = query;
+	  	});
+	}
+
 	render() {
 		return(
 			<div className="search-books">
 			  <div className="search-books-bar">
 			  	<Link to="/" className="close-search">Close</Link>
 			    <div className="search-books-input-wrapper">
-			      <input type="text" placeholder="Search by title or author"/>
+			      <input
+			      	type="text"
+			      	placeholder="Search by title or author"
+			      	value={this.state.query}
+			      	onChange={ e => {this.searchBooks(e.target.value)}}
+			      	/>
 			    </div>
 			  </div>
 			  <div className="search-books-results">
 			    <ol className="books-grid">
-			    	{this.state.foundBooks.map((book) => (
+			    	{this.state.searchedBooks.map((book) => (
 			    	    <BookItem key={book.title} book={book}/>
 			    	))}
 			    </ol>
