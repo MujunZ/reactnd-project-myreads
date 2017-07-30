f4160c2d2e59834b7539a5fbd2fd7c52d649fb78
MujunZ <o.6180339@gmail.com>
2017-07-30 15:23:08 -0400

    Set Book Shelf in Search Result to be "none"

--- src/BookItem.js
+++ src/BookItem.js
@@ -11,7 +11,7 @@ class BookItem extends Component {
 			      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks})` }}></div>
 			      <div className="book-shelf-changer">
 			        <select onChange={e => this.props.onMoveShelf(e,book)} value={book.shelf}>
-			          <option value="none" disabled>Move to...</option>
+			          <option value="" disabled>Move to...</option>
 			          <option value="currentlyReading">Currently Reading</option>
 			          <option value="wantToRead">Want to Read</option>
 			          <option value="read">Read</option>
